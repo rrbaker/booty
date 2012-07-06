ca154e557ddd73a05505c78dfb5c84717f24f661
Rob Baker <rrbaker@gmail.com>
2012-07-06 09:52:03 -0400

    restarting header and footer views for Uv2.4

--- views/footer.php
+++ views/footer.php
@@ -1,73 +1,84 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-<head>
-	<title><?php echo $site_name; ?></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<?php echo $header_block; ?>
-	<?php
-	// Action::header_scripts - Additional Inline Scripts from Plugins
-	Event::run('ushahidi_action.header_scripts');
-	?>
-</head>
-
-<body id="page">
-	<!-- wrapper -->
-	<div class="rapidxwpr floatholder">
-
-		<!-- header -->
-		<div id="header">
-
-			<!-- searchbox -->
-			<div id="searchbox">
-				
-				<!-- user actions -->
-				<div id="loggedin_user_action" class="clearingfix">
-					<?php if($loggedin_username != FALSE){ ?>
-						<a href="<?php echo url::site().$loggedin_role;?>"><?php echo $loggedin_username; ?></a> [<a href="<?php echo url::site();?>logout/front"><?php echo Kohana::lang('ui_admin.logout');?></a>]
-					<?php } else { ?>
-						<a href="<?php echo url::site()."members/";?>"><?php echo Kohana::lang('ui_main.login'); ?></a>
-					<?php } ?>
-				</div><br/>
-				<!-- / user actions -->
-				
-				<!-- languages -->
-				<?php echo $languages;?>
-				<!-- / languages -->
-
-				<!-- searchform -->
-				<?php echo $search; ?>
-				<!-- / searchform -->
-
 			</div>
-			<!-- / searchbox -->
-			
-			<!-- logo -->
-			<?php if($banner == NULL){ ?>
-			<div id="logo">
-				<h1><a href="<?php echo url::site();?>"><?php echo $site_name; ?></a></h1>
-				<span><?php echo $site_tagline; ?></span>
-			</div>
-			<?php }else{ ?>
-			<a href="<?php echo url::site();?>"><img src="<?php echo url::base().Kohana::config('upload.relative_directory')."/".$banner; ?>" alt="<?php echo $site_name; ?>" /></a>
-			<?php } ?>
-			<!-- / logo -->
-			
-			<!-- submit incident -->
-			<?php echo $submit_btn; ?>
-			<!-- / submit incident -->
-			
 		</div>
-		<!-- / header -->
+		<!-- / main body -->
 
-		<!-- main body -->
-		<div id="middle">
-			<div class="background layoutleft">
+	</div>
+	<!-- / wrapper -->
 
-				<!-- mainmenu -->
-				<div id="mainmenu" class="clearingfix">
-					<ul>
-						<?php nav::main_tabs($this_page); ?>
-					</ul>
+	<!-- footer -->
+	<div id="footer" class="clearingfix">
 
-				</div>
-				<!-- / mainmenu -->
+		<div id="underfooter"></div>
+
+		<!-- footer content -->
+		<div class="rapidxwpr floatholder">
+
+			<!-- footer credits -->
+			<div class="footer-credits">
+				Powered by the &nbsp;
+				<a href="http://www.ushahidi.com/">
+					<img src="<?php echo url::file_loc('img'); ?>media/img/footer-logo.png" alt="Ushahidi" style="vertical-align:middle" />
+				</a>
+				&nbsp; Platform
+			</div>
+			<!-- / footer credits -->
+
+			<!-- footer menu -->
+			<div class="footermenu">
+				<ul class="clearingfix">
+					<li>
+						<a class="item1" href="<?php echo url::site(); ?>">
+							<?php echo Kohana::lang('ui_main.home'); ?>
+						</a>
+					</li>
+
+					<?php if (Kohana::config('settings.allow_reports')): ?>
+					<li>
+						<a href="<?php echo url::site()."reports/submit"; ?>">
+							<?php echo Kohana::lang('ui_main.submit'); ?>
+						</a>
+					</li>
+					<?php endif; ?>
+					
+					<?php if (Kohana::config('settings.allow_alerts')): ?>
+						<li>
+							<a href="<?php echo url::site()."alerts"; ?>">
+								<?php echo Kohana::lang('ui_main.alerts'); ?></a>
+						</li>
+					<?php endif; ?>
+
+					<?php if (Kohana::config('settings.site_contact_page')): ?>
+					<li>
+						<a href="<?php echo url::site()."contact"; ?>">
+							<?php echo Kohana::lang('ui_main.contact'); ?>
+						</a>
+					</li>
+					<?php endif; ?>
+
+					<?php
+					// Action::nav_main_bottom - Add items to the bottom links
+					Event::run('ushahidi_action.nav_main_bottom');
+					?>
+				</ul>
+
+				<?php if ($site_copyright_statement != ''): ?>
+	      		<p><?php echo $site_copyright_statement; ?></p>
+		      	<?php endif; ?>
+		      	
+			</div>
+			<!-- / footer menu -->
+
+
+		</div>
+		<!-- / footer content -->
+
+	</div>
+	<!-- / footer -->
+
+	<?php
+	echo $footer_block;
+	// Action::main_footer - Add items before the </body> tag
+	Event::run('ushahidi_action.main_footer');
+	?>
+</body>
+</html>
