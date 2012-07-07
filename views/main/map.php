734e665b7796110b4c7282ea7f5883c6463c57fa
Rob Baker <rrbaker@gmail.com>
2012-07-07 14:44:06 -0400

    bringing over nav, main, and reports views from default theme

---
+++ views/main/map.php
@@ -0,0 +1,7 @@
+<div class="map" id="map"></div>
+<div id="mapStatus">
+	<div id="mapScale" style="border-right: solid 1px #999"></div>
+	<div id="mapMousePosition" style="min-width: 135px;border-right: solid 1px #999;text-align: center"></div>
+	<div id="mapProjection" style="border-right: solid 1px #999"></div>
+	<div id="mapOutput"></div>
+</div> <!-- /map -->
