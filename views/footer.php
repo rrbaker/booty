33914ed9d3ea6971e651f1df37c4302b61e31bf5
Rob Baker <rrbaker@gmail.com>
2012-07-24 16:10:42 -0400

    cleaned up footer; new logo

--- views/footer.php
+++ views/footer.php
@@ -1,55 +1,51 @@
 
 		<footer id="footer" role="contentinfo">
 			<div id="footer_inner" class="row">
-				<!-- footer menu -->
-				<nav role="navigation" class="nav-footer span8">
-					<ul class="unstyled">
-						<li>
-							<a class="item1" href="<?php echo url::site(); ?>">
-								<?php echo Kohana::lang('ui_main.home'); ?>
-							</a>
-						</li>
-
-						<?php if (Kohana::config('settings.allow_reports')): ?>
-						<li>
-							<a href="<?php echo url::site()."reports/submit"; ?>">
-								<?php echo Kohana::lang('ui_main.submit'); ?>
-							</a>
-						</li>
-						<?php endif; ?>
-						
-						<?php if (Kohana::config('settings.allow_alerts')): ?>
+				<div class="span8">
+					<nav role="navigation" class="nav-footer">
+						<ul class="unstyled">
 							<li>
-								<a href="<?php echo url::site()."alerts"; ?>">
-									<?php echo Kohana::lang('ui_main.alerts'); ?></a>
+								<a class="item1" href="<?php echo url::site(); ?>">
+									<?php echo Kohana::lang('ui_main.home'); ?>
+								</a>
 							</li>
-						<?php endif; ?>
 
-						<?php if (Kohana::config('settings.site_contact_page')): ?>
-						<li>
-							<a href="<?php echo url::site()."contact"; ?>">
-								<?php echo Kohana::lang('ui_main.contact'); ?>
-							</a>
-						</li>
-						<?php endif; ?>
-
-						<?php
-						// Action::nav_main_bottom - Add items to the bottom links
-						Event::run('ushahidi_action.nav_main_bottom');
-						?>
-					</ul>
-
-					<?php if ($site_copyright_statement != ''): ?>
-							<p><?php echo $site_copyright_statement; ?></p>
+							<?php if (Kohana::config('settings.allow_reports')): ?>
+							<li>
+								<a href="<?php echo url::site()."reports/submit"; ?>">
+									<?php echo Kohana::lang('ui_main.submit'); ?>
+								</a>
+							</li>
 							<?php endif; ?>
-				</div> <!-- /nav-footer -->
+							
+							<?php if (Kohana::config('settings.allow_alerts')): ?>
+								<li>
+									<a href="<?php echo url::site()."alerts"; ?>">
+										<?php echo Kohana::lang('ui_main.alerts'); ?></a>
+								</li>
+							<?php endif; ?>
+
+							<?php if (Kohana::config('settings.site_contact_page')): ?>
+							<li>
+								<a href="<?php echo url::site()."contact"; ?>">
+									<?php echo Kohana::lang('ui_main.contact'); ?>
+								</a>
+							</li>
+							<?php endif; ?>
+
+							<?php
+							// Action::nav_main_bottom - Add items to the bottom links
+							Event::run('ushahidi_action.nav_main_bottom');
+							?>
+						</ul>
+					</nav>
+					<?php if ($site_copyright_statement != ''): ?>
+						<p><?php echo $site_copyright_statement; ?></p>
+					<?php endif; ?>
+				</div> <!-- /span8 -->
 
 				<div class="credit span4">
-					Powered by the &nbsp;
-					<a href="http://www.ushahidi.com/">
-						<img src="<?php echo url::file_loc('img'); ?>media/img/footer-logo.png" alt="Ushahidi" style="vertical-align:middle" />
-					</a>
-					&nbsp; Platform
+					<p>Powered by the <a href="http://ushahidi.com/"><img src="/themes/booty/img/footer-logo.png" alt="Ushahidi logo" /></a> Platform</p>
 				</div>
 			</div>
 		</footer> <!-- /footer -->
